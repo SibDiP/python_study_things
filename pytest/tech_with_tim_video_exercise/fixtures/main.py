e42ad7801444a05b6ef6f82cb080420a016763c3
sibdip <tskdip@gmail.com>
2026-03-13 20:35:36 +0700

    pytest_fixtures: finish CRUD

--- pytest/tech_with_tim_video_exercise/fixtures/main.py
+++ pytest/tech_with_tim_video_exercise/fixtures/main.py
@@ -1,40 +1,98 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, select, exists
+from sqlalchemy.orm import Session
 from models import Base, User
-from fixtures.exceptions import UserAlreadyExistsError
+from fixtures.exceptions import UserAlreadyExistsError, UserNotFoundError
 
+# echo=True - SQL логи
 engine = create_engine('sqlite:///users.db', echo=True)
-
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+class UserCRUD:
+    # Depency Enjection для удобства тестов.
+    def __init__(self,session: Session):
+        self.session = session
 
-user = User(username="john_doe", email="john@example.com")
-session.add(user)
-session.commit()
-
-print("✅ БД создана! Пользователь добавлен!")
-print(f"ID: {user.id}")
-session.close()
-
-
-
-# class UserManager:
-#     def __init__(self):
-#         self.users = {}
+    def _user_exist(self,email: str) -> bool:
+        """Проверить наличие пользователя в БД по уникальному email
+        (name не предполагает уникальности)."""
+        # Разделение логики:
+            # что хотим получить
+        stmt = select(exists().where(User.email == email))
+            # выполняем запрос
+        return self.session.execute(stmt).scalar()
     
-#     def add_user(self, username, email):
-#         if username in self.users:
-#             raise ValueError("User already exists")
-#         self.users[username] = email
-#         return True
+    def user_create_or_raise(self, name: str, email: str) -> User:
+        """Создать User в БД
+
+        Args:
+            name (str): имя пользовтаеля
+            email (str): email пользователя
+
+        Raises:
+            UserAlreadyExistsError: ошибка при наличии в БД пользователя с переданным email
+
+        Returns:
+            User: объект User с актуальными БД параметрами
+        """
+        if self._user_exist(email):
+            raise UserAlreadyExistsError(email)
+        
+        new_user = User(username=name, email=email)
+        # подготовка
+        self.session.add(new_user)
+        # сохранить в БД
+        self.session.commit()
+        # подтянуть в объект User все данные (включя генерируемые) из БД.
+        self.session.refresh(new_user)
+        return new_user
     
-#     def get_user(self, username): # return None if nonexist
-#         return self.users.get(username)
+    def user_get(self, email: str) -> User | None:
+        """Вернуть User с указанным email если он есть в БД.
+
+        Returns:
+            User | None:
+        """
+        stmt = select(User).where(User.email == email)
+        # scalar_one_or_none() - стандарт для Unique поля
+        return self.session.execute(stmt).scalar_one_or_none()
     
-#     def get_user_or_raise(self, username): # raise KeyError if nonexist
-#         email = self.get_user(username)
-#         if email is None:
-#             raise KeyError(f"User '{username}' not found")
-#         return email
+    def user_update_or_raise(self, email: str, **kwargs) -> User:
+        """Обновить данные объекта User в БД
+
+        Args:
+            email (str): email пользователя
+
+        Raises:
+            UserNotFoundError: ошибка при отсутсвии пользователя с указанным email в БД
+
+        Returns:
+            User:  
+        """
+        user = self.user_get(email)
+
+        if not user:
+            raise UserNotFoundError(email)
+        
+        
+        for k,v in kwargs.items():
+            if hasattr(user, k):
+                setattr(user, k, v)
+        
+        self.session.commit()
+        self.session.refresh(user)
+        return user
+
+        
+    def user_delite(self, email: str) -> bool:
+        """Удалить пользователя с указанным email из БД
+
+        Returns:
+            bool: 
+        """
+        user = self.user_get(email)
+        if user:
+            self.session.delete(user)
+            self.session.commit()
+            return True
+        return False
+    
