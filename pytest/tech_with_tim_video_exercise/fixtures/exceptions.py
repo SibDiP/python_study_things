65fdd69f1a975f0380d62eaae7cece32027a673b
sibdip <tskdip@gmail.com>
2026-03-13 20:35:47 +0700

    pytest_fixtures: fix exceptions

--- pytest/tech_with_tim_video_exercise/fixtures/exceptions.py
+++ pytest/tech_with_tim_video_exercise/fixtures/exceptions.py
@@ -2,7 +2,7 @@ class BaseFixturesError(Exception):
     """Базовое исключение, что б ловить все ошибки приложения"""
     pass
 
-class UserAlreadyExistsError(BaseException):
+class UserAlreadyExistsError(BaseFixturesError):
     """Ошибка: пользователь с таким email уже существует
 
     Args:
@@ -13,3 +13,13 @@ class UserAlreadyExistsError(BaseException):
         self.message = f"Пользователь с email {email} уже существует в БД."
         super().__init__(self.message)
 
+class UserNotFoundError(BaseFixturesError):
+    """Ошибка: пользовтель с таким email отсутсвует в БД
+    
+    Args:
+        email (str): email пользователя
+    """
+    def __init__(self, email: str):
+        self.email = email
+        self.message = f"Пользователь с email {email} отсутствует в БД"
+        super().__init__(self.message)
