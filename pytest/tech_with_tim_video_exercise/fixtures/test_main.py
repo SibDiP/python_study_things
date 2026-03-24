c5f048de4f60acb436f1d76e33392f240984ef94
sibdip <tskdip@gmail.com>
2026-03-24 12:11:48 +0700

    pytest_fixtures: refacotring. Add actual UserCRUD

--- pytest/tech_with_tim_video_exercise/fixtures/test_main.py
+++ pytest/tech_with_tim_video_exercise/fixtures/test_main.py
@@ -1,22 +1,35 @@
 import pytest
-from main import UserManager
+from main import UserCRUD
+from exceptions import BaseFixturesError, UserAlreadyExistsError, UserNotFoundError
+from sqlalchemy.orm import sessionmaker
 
 @pytest.fixture
-def user_manager():
-    """Creates a fresh instance of UserManager before each test."""
-    return UserManager()
+def user_CRUD():
+    """Creates a fresh instance of UserCRUD before each test."""
+    SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+    return UserCRUD(SessionLocal)
 
-def test_adds_user_successfully(user_manager):
-    assert user_manager.add_user("john_doe", "john@example.com")
-    assert user_manager.get_user("john_doe") == "john@example.com"
+def test_adds_user_successfully(user_CRUD):
+    assert user_CRUD.user_create_or_raise("john_doe", "john@example.com")
+    assert user_CRUD.user_get("john@example.com") == 
 
-def test_raises_error_on_duplicate_username(user_manager):
-    user_manager.add_user("john_doe", "john@example.com")
-    with pytest.raises(ValueError):
-        user_manager.add_user("john_doe", "another@example.com")
 
-def test_raises_error_on_getting_non_exist_user(user_manager):
+
+
+
+
+
+
+
+
+
+def test_raises_error_on_duplicate_username(user_CRUD):
+    user_CRUD.user_create_or_raise("john_doe", "john@example.com")
+    with pytest.raises(UserAlreadyExistsError):
+        user_CRUD.user_create_or_raise("john_doe", "another@example.com")
+
+def test_raises_error_on_getting_non_exist_user(user_CRUD):
     user_name = "john_doe"
     with pytest.raises(KeyError, match=f"User '{user_name}' not found"):
-        user_manager.get_user_or_raise(user_name)
+        user_CRUD.get_user_or_raise(user_name)
         
