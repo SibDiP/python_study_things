3d539ebdab173cc89133e945a976e9b3bfd77d19
sibdip <tskdip@gmail.com>
2026-07-01 13:11:24 +0700

    rename 'patterns' folder to 'patter'. Add 'timeit.py' decorator.

---
+++ patterns/decorator/timeit.py
@@ -0,0 +1,33 @@
+"""
+Задание 3: Написание декоратора
+
+Напиши декоратор @timeit, который измеряет время выполнения функции и выводит 
+его на экран.
+Подсказка: используй модуль time и функцию time.time().
+Не забудь про functools.wraps!
+
+Примени этот декоратор к функции, которая выполняет какую-то долгую операцию 
+(например, цикл на миллион итераций).
+"""
+
+import time
+from functools import wraps
+
+
+def timeit(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        print(f"Запуск {func.__name__}")
+        result = func(*args, **kwargs)
+        duration = time.perf_counter() - start_time
+        print(f"Время выполнения {func.__name__}: {duration:.4f}")
+        return result
+    return wrapper
+
+@timeit
+def slow_func(sec) -> str:
+    time.sleep(sec)
+    return f"Oh, I was sleep for a {sec}"
+
+slow_func(1)
