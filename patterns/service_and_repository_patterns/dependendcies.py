0dff76942bc237384894f0046de64e8fd23333d1
sibdip <tskdip@gmail.com>
2026-07-08 15:54:50 +0700

    Add CRUD for InMemoryNoteRepository class

--- patterns/service_and_repository_patterns/dependendcies.py
+++ patterns/service_and_repository_patterns/dependendcies.py
@@ -19,11 +19,30 @@ class InMemoryNoteRepository(AbstractNoteRepository):
         self._notes : dict[int, Note] = {}
         self._next_id: int = 1
     
-    def create_note(self, note = NoteIn) -> Note:
+    def create_note(self, note_in: NoteIn) -> Note:
         new_note = Note(
             id=self._next_id, 
-            title=note.title, 
-            content=note.content
+            **note_in.model_dump(),
             )
         self._notes[self._next_id] = new_note
         return new_note
+    
+    def get_all_notes(self) -> list[Note]:
+        return list(self._notes.values())
+    
+    def get_note_by_id(self, note_id: int) -> Note:
+        #TODO. Проверка на наличие
+        return self._notes[note_id]
+    
+    def update_note(self, note_id: int, note_in: NoteIn) -> Note:
+        update_note = Note(
+            id=note_id,
+            **note_in.model_dump(),
+            )
+        self._notes[update_note.id] = update_note
+
+        return update_note
+    
+    def del_note(self, note_id: int) -> None:
+        self._notes.pop(note_id)
+        return None
